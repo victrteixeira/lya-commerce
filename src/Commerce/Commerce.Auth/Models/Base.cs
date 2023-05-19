ed910a4d9626fe0c6c0e0e2cd0cad15f62e62e5f
Victor Teixeira <vicktorteixeira.7@gmail.com>
2023-05-19 18:55:28 -0300

    feature: password hasher and user validations for authentication project

---
+++ src/Commerce/Commerce.Auth/Models/Base.cs
@@ -0,0 +1,62 @@
+﻿using System.Security.Cryptography;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace Commerce.Auth.Models;
+
+public abstract class Base
+{
+    // User Validation Segment
+    private List<string> _errors;
+    public IReadOnlyCollection<string> Errors => _errors;
+    private void AddErrorList(IList<ValidationFailure> errors)
+    {
+        foreach (var error in errors)
+        {
+            _errors.Add(error.ErrorMessage);
+        }
+    }
+
+    public bool IsValid() => _errors.Count == 0;
+    
+    protected bool Validate<T, J>(T validator, J obj) where T : AbstractValidator<J>
+    {
+        var validation = validator.Validate(obj);
+
+        if (validation.Errors.Count > 0)
+            AddErrorList(validation.Errors);
+
+        return IsValid();
+    }
+    
+    // Password Hash Segment
+    private const int _saltSize = 16;
+    private const int _keySize = 32;
+    private const int _iterations = 50000;
+    private static readonly HashAlgorithmName _algorithm = HashAlgorithmName.SHA256;
+
+    private const char segmentDelimiter = ':';
+    
+    protected string EncryptPassword(string input)
+    {
+        byte[] salt = RandomNumberGenerator.GetBytes(_saltSize);
+        byte[] hash = Rfc2898DeriveBytes.Pbkdf2(input, salt, _iterations, _algorithm, _keySize);
+
+        return string.Join(segmentDelimiter, Convert.ToHexString(hash), Convert.ToHexString(salt), _iterations,
+            _algorithm);
+    }
+
+    protected bool VerifyHash(string input, string hashString)
+    {
+        string[] segments = hashString.Split(segmentDelimiter);
+        byte[] hash = Convert.FromHexString(segments[0]);
+        byte[] salt = Convert.FromHexString(segments[1]);
+        int iterations = int.Parse(segments[2]);
+
+        HashAlgorithmName algorithm = new HashAlgorithmName(segments[3]);
+
+        byte[] inputHash = Rfc2898DeriveBytes.Pbkdf2(input, salt, iterations, algorithm, hash.Length);
+
+        return CryptographicOperations.FixedTimeEquals(inputHash, hash);
+    }
+}
---
+++ src/Commerce/Commerce.Auth/Validators/UserValidator.cs
@@ -0,0 +1,58 @@
+﻿using Commerce.Auth.Models;
+using FluentValidation;
+
+namespace Commerce.Auth.Validators;
+
+public class UserValidator : AbstractValidator<User>
+{
+    public UserValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotNull().WithMessage("O primeiro nome não pode ser nulo.")
+            .NotEmpty().WithMessage("O primeiro nome não pode ficar vazio.")
+            .Length(1, 20).WithMessage("No máximo 20 caracteres são permitidos.")
+            .NotEqual(y => y.LastName).WithMessage("O primeiro nome não pode ser igual ao seu último nome.")
+            .Matches(@"^[a-zA-Z0-9]+$").WithMessage("Apenas letras e números são permitidos.");
+        
+        RuleFor(x => x.LastName)
+            .NotNull().WithMessage("O último nome não pode ser nulo.")
+            .NotEmpty().WithMessage("O último nome não pode ficar vazio.")
+            .Length(1, 20).WithMessage("No máximo 20 caracteres são permitidos.")
+            .Matches(@"^[a-zA-Z0-9]+$").WithMessage("Apenas letras e números são permitidos.");
+
+        RuleFor(x => x.EmailAddress)
+            .NotNull().WithMessage("O e-mail não pode ser nulo.")
+            .NotEmpty().WithMessage("O e-mail não pode ficar vazio.")
+            .Custom((email, context) =>
+            {
+                var trimmedEmail = email.Trim();
+
+                if (trimmedEmail.EndsWith("."))
+                {
+                    context.AddFailure("O Email não deve terminar com ponto.");
+                }
+
+                try
+                {
+                    var _ = new System.Net.Mail.MailAddress(email);
+                }
+                catch
+                {
+                    context.AddFailure("O email não é válido.");
+                }
+            });
+
+        RuleFor(x => x.Password)
+            .Custom((pwd, context) =>
+            {
+                bool isValid = pwd.Any(char.IsUpper)
+                               && pwd.Any(char.IsLower)
+                               && pwd.Any(char.IsDigit)
+                               && pwd.Contains(' ')
+                               && pwd.Length >= 8;
+                
+                if (!isValid)
+                    context.AddFailure("Senha não é válida. Precisa conter ao menos uma letra maiúscula, uma minúscula, um digito númerico, e precisa ser maior ou igual a 8 digitos.");
+            });
+    }
+}
