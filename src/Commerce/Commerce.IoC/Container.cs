e17287e925d2ebeeed0760ed6a95f6d1a51278e3
Victor Teixeira <vicktorteixeira.7@gmail.com>
2023-05-20 17:20:27 -0300

    feat: project to perform inversion of control

---
+++ src/Commerce/Commerce.IoC/Container.cs
@@ -0,0 +1,14 @@
+﻿using Commerce.Security.Utils;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace Commerce.IoC;
+
+public static class Container
+{
+    public static IServiceCollection AddSecurity(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAutoMapper(typeof(AuthMapper));
+        return services;
+    }
+}
