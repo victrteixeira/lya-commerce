8e98879d5bbcf08fcfbd52d1d12cb84d4cfdf4a6
Victor Teixeira <vicktorteixeira.7@gmail.com>
2023-05-25 23:48:31 -0300

    feat: interface and repository implementation

---
+++ src/projects/Commerce/Commerce.Security/Interfaces/ISecurityRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using Commerce.Security.DTOs;
+using Commerce.Security.Models;
+
+namespace Commerce.Security.Interfaces;
+
+public interface ISecurityRepository
+{
+    Task<IReadOnlyCollection<User>> GetAllUsers();
+    Task<User?> GetSingleUserAsync(int id); // TODO -> Check out whether this will stay as integer.
+    Task AddUserAsync(User newUser);
+    Task UpdateUserAsync(int id, User updatedUser);
+    Task DeleteUserAsync(int id);
+}
---
+++ src/projects/Commerce/Commerce.Security/Repository/SecurityRepository.cs
@@ -0,0 +1,41 @@
+﻿using Commerce.Security.Database;
+using Commerce.Security.DTOs;
+using Commerce.Security.Interfaces;
+using Commerce.Security.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Commerce.Security.Repository;
+
+public class SecurityRepository : ISecurityRepository
+{
+    private readonly IMongoCollection<User> _userCollection;
+
+    public SecurityRepository(IOptions<MongoDbSettings> securityDatabaseSettings)
+    {
+        var mongoClient = new MongoClient(
+            securityDatabaseSettings.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(
+            securityDatabaseSettings.Value.DatabaseName);
+
+        _userCollection = mongoDatabase.GetCollection<User>(
+            securityDatabaseSettings.Value.UserCollectionName);
+    }
+
+
+    public async Task<IReadOnlyCollection<User>> GetAllUsers() =>
+        await _userCollection.Find(_ => true).ToListAsync();
+
+    public async Task<User?> GetSingleUserAsync(int id) =>
+        await _userCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+    public async Task AddUserAsync(User newUser) =>
+        await _userCollection.InsertOneAsync(newUser);
+
+    public async Task UpdateUserAsync(int id, User updatedUser) =>
+        await _userCollection.ReplaceOneAsync(x => x.Id == id, updatedUser);
+
+    public async Task DeleteUserAsync(int id) =>
+        await _userCollection.DeleteOneAsync(x => x.Id == id);
+}
