d6392cfc27ac419958432c2f0f90a2a631cfead3
Ian de Souza Pimentel <contato.iansouzapimentel@gmail.com>
2023-05-26 15:58:42 -0300

    feat: apply validations and product constructor

--- src/projects/Commerce/Commerce.Core/Entities/Product.cs
+++ src/projects/Commerce/Commerce.Core/Entities/Product.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Commerce.Core.Validations;
 
 namespace Commerce.Core.Entities
 {
     public class Product
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public string Manufacturer { get; set; }
-        public string Category { get; set; }
-        public string SubCategory { get; set; }
+        public Product(int id, string name, string description, decimal price, string category, string? manufacturer = null, string? subcategory = null) { 
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+            Category = category;
+            SubCategory = subcategory;
+            Manufacturer = manufacturer;
+            Validate();
+        }
+
+        public int Id { get; private set; }
+        public string Name { get; }
+        public string Description { get; }
+        public decimal Price { get; }
+        public string? Manufacturer { get; }
+        public string Category { get; }
+        public string? SubCategory { get; }
+
+        public bool Validate() => new ProductValidator().Validate(this).IsValid;
     }
 }
