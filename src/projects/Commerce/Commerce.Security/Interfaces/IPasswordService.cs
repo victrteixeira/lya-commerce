3b43e91f0fddac43e0f86383e7a5c15678406791
Victor Teixeira <vicktorteixeira.7@gmail.com>
2023-05-27 00:39:51 -0300

    feat: service layer interfaces

---
+++ src/projects/Commerce/Commerce.Security/Interfaces/IPasswordService.cs
@@ -0,0 +1,8 @@
+﻿namespace Commerce.Security.Interfaces;
+
+public interface IPasswordService
+{
+    Task<string> EncryptPassword(string input);
+    bool VerifyHash(string input, string hashPwd);
+    bool ValidatePassword(string pwd);
+}
---
+++ src/projects/Commerce/Commerce.Security/Interfaces/ISecurityService.cs
@@ -0,0 +1,9 @@
+﻿using Commerce.Security.DTOs;
+
+namespace Commerce.Security.Interfaces;
+
+public interface ISecurityService
+{
+    Task<ReadUser?> RegisterAsync(CreateUser command);
+    Task<bool> LoginAsync(LoginUser command);
+}
