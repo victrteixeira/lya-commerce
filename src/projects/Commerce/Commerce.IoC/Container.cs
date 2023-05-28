8d3d7c56252e31eae79d212492b6450ae17ec734
Victor Teixeira <vicktorteixeira.7@gmail.com>
2023-05-27 22:38:08 -0300

    feat: Dependency Injection

--- src/projects/Commerce/Commerce.IoC/Container.cs
+++ src/projects/Commerce/Commerce.IoC/Container.cs
@@ -1,8 +1,12 @@
-﻿using Commerce.Security.Database;
+﻿using Commerce.Core.Interfaces;
+using Commerce.Infrastructure.Database;
+using Commerce.Infrastructure.Repositories;
+using Commerce.Security.Database;
 using Commerce.Security.Interfaces;
 using Commerce.Security.Repository;
 using Commerce.Security.Services;
 using Commerce.Security.Utils;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 
@@ -20,6 +24,16 @@ public static class Container
         services.AddScoped<ISecurityService, SecurityService>();
         
         services.AddAutoMapper(typeof(AuthMapper));
+        return services;
+    }
+
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connStr = configuration.GetConnectionString("MySqlDatabaseConfig");
+        services.AddDbContext<CommerceContext>(opt => opt.UseMySql(connStr, ServerVersion.AutoDetect(connStr)));
+
+        services.AddScoped<IProductRepository, ProductRepository>();
+
         return services;
     }
 }
