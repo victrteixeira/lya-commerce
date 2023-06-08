0e63952328599f6d959dc2a72433510bc952fe6c
Victor Teixeira <vicktorteixeira.7@gmail.com>
2023-06-07 21:06:13 -0300

    refac: validate password method made private

--- src/projects/Commerce/Commerce.Security/Interfaces/IPasswordService.cs
+++ src/projects/Commerce/Commerce.Security/Interfaces/IPasswordService.cs
@@ -4,8 +4,7 @@ namespace Commerce.Security.Interfaces;
 
 public interface IPasswordService
 {
-    Task<string> EncryptPasswordAsync(string input);
-    Task<User> UpdatePasswordAsync(string passwordStored, User user, string password);
-    bool VerifyHash(string input, string hashPwd);
-    bool ValidatePassword(string pwd);
+    string HashPassword(string input);
+    bool ValidateHash(string input, string hashPwd);
+    User UpdateHash(string passwordStored, User user, string password);
 }
